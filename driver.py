2380f86bec50f53ee3d09270a0441cb9ceafe504
Martin Barnes <brusebox2000@gmail.com>
2016-04-15 09:30:58 +0100

    Added changes to cycle through databases and output to csv

--- driver.py
+++ driver.py
@@ -1,6 +1,4 @@
-import subprocess
-import re
-import sys
+import time, csv, re, subprocess
 
 
 def run_command(command):
@@ -9,24 +7,48 @@ def run_command(command):
                          stderr=subprocess.STDOUT)
     return iter(p.stdout.readline, b'')
 
-searcharea = sys.argv[1]
 
-command = ("hadoop fsck " + searcharea + " -files -blocks -locations").split()
+databasescommand = ("hadoop fs -ls /user/hive/warehouse/*.db").split()
+date = time.strftime("%d/%m/%Y")
 
-ips = []
+outputs = []
 
-for line in run_command(command):
-	ip = re.findall('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\:[0-9]{2,5})', line)
-	if(ip):
-		ips.append(ip)
+def getBlockDistribution(database):
+    command = ("hadoop fsck " + database + " -files -blocks -locations").split()
 
-ipcounts = {}
+    ips = []
 
-for ip in ips:
-	if(ipcounts.has_key(str(ip))):
-		ipcounts[str(ip)] += 1
-	else:
-		ipcounts[str(ip)] = 1
 
-for key, value in ipcounts.iteritems():
-	print "Datanode %s has %d blocks" % (str(key), value)
+    for line in run_command(command):
+        ip = re.findall('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\:[0-9]{2,5})', line)
+        if (ip):
+            ips.append(ip)
+
+    ipcounts = {}
+
+    ipsfinal = []
+
+    for ip in ips:
+        for i in ip:
+            ipsfinal.append(i)
+
+    for ip in ipsfinal:
+        if (ipcounts.has_key(str(ip))):
+            ipcounts[str(ip)] += 1
+        else:
+            ipcounts[str(ip)] = 1
+
+    for key, value in ipcounts.iteritems():
+        return (key, value)
+
+for db in run_command(databasescommand):
+
+    temparr = re.findall('(\/user\/hive\/warehouse\/.*)$', db)
+    if (temparr):
+        for dbitem in temparr:
+            tple = getBlockDistribution(dbitem)
+            outputs.append((date, temparr, tple[0], tple[1]))
+
+csvwriter = csv.writer(open('blockdistibution.csv', 'wb'), delimiter=',')
+for output in outputs:
+    csvwriter.writerow(output)
